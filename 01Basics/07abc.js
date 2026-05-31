eca813310ff231d2eed901d95c57d4b52c00ea16
GHANNATH KUMAR <ghannathk@gmail.com>
2026-05-31 02:24:59 +0000

    uodate it

--- 01Basics/07abc.js
+++ 01Basics/07abc.js
@@ -40,3 +40,43 @@ console.log(typeof name) // string
 
  // splice-> manupulates original array...
  
+
+
+ const marvel_heros=["ghannath" , "kumar"]
+ const marvel_heros2=["abc" ,"ghf"]
+ marvel_heros.push(marvel_heros2)
+ console.log(marvel_heros) // [ 'ghannath', 'kumar', [ 'abc', 'ghf' ] ]
+ // so here an array has been pushed..-> not merged 
+
+ const marvel_heros3=["jkl","mki"]
+ marvel_heros3.concat(marvel_heros) 
+ console.log( marvel_heros3.concat(marvel_heros))  // but not chane array->  'jkl', 'mki', 'ghannath', 'kumar', [ 'abc', 'ghf' ] ]
+ console.log(marvel_heros3) 
+
+ const x=[1,2,3]
+ const y=[3,4,5]
+ x.push(y) // chnaged atual array 
+ console.log(x)
+ const z=x.concat(["ghj","klo"])
+ console.log(z)
+
+ // work of spred in js
+
+ const k=[1,2,3,4]
+ const l=[7,8,9,10,11,12,13]
+ const p=[...k,...l] // all elemtns of k and l spreadly will come in p
+ console.log(p) // [1, 2,  3,  4,  7, 8, 9, 10, 11, 12,13] 
+
+ // flat 
+ const r=[1,2,3,4,[4,5,6,7],[3,[4,5,6,7,[9,10,11,12,13,]]]]
+ const f=r.flat(Infinity) 
+ console.log(f)  // all element of array individually will come int it 
+
+
+ console.log(Array.isArray("ghannath")) // false 
+ console.log(Array.from("Hitesh")) //[ 'H', 'i', 't', 'e', 's', 'h' ]
+ console.log(Array.from({name: "Hitesh"})) // []  empty array becuase they do not find ki key ko aaray banana hhai ya fir value koo  , so isko batana padt hai 
+
+ let score1=100
+ let score2=300
+ console.log(Array.of(score1,score2)) // [100,300]
